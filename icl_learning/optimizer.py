a2e4681b86623724264ccce34795a3e13ea6405a
N-Liniger <noah.liniger@protonmail.com>
2024-05-28 11:07:26 +0200

    L1 loss (also some diff optimizers I tried)

--- icl_learning/optimizer.py
+++ icl_learning/optimizer.py
@@ -5,5 +5,9 @@ def select_optim(config, model: torch.nn.Module):
         return torch.optim.Adam(lr=config["optimizer"]["lr"], params=model.parameters())
     elif config["optimizer"]["type"] == "sgd":
         return torch.optim.SGD(lr=config["optimizer"]["lr"], params=model.parameters())
+    elif config["optimizer"]["type"] == "sgd_momentum":
+        return torch.optim.SGD(lr=config["optimizer"]["lr"], momentum=0, params=model.parameters())
+    elif config["optimizer"]["type"] == "adamw":
+        return torch.optim.AdamW(lr=config["optimizer"]["lr"], params=model.parameters())
     else:
         raise ValueError("Optimizer not supported")
